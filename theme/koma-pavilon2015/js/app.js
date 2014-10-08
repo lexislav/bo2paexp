af43c083f026979a04fad42ef9b0fc93ff05039e
Michal Hotovec <michal.hotovec@gmail.com>
2014-10-08 17:46:28 +0200

    prehozeni js do global app.js

--- theme/koma-pavilon2015/js/app.js
+++ theme/koma-pavilon2015/js/app.js
@@ -4,6 +4,28 @@
 
 $(function () {
 
+
+    var menuBreakPoint = $(window).height() * .9;
+
+    $(window).resize(function () {
+        menuBreakPoint = $(window).height() * .9;
+    });
+
+    // menu swithching
+    if( $("html").hasClass("no-touch")) {
+        $(window).scroll(function () {
+            console.log('st:' + $(window).scrollTop());
+            if ($(window).scrollTop() > menuBreakPoint) {
+                $(".m-nav").addClass("mm-white");
+                $(".m-nav").removeClass("mm-black");
+            } else {
+                $(".m-nav").addClass("mm-black");
+                $(".m-nav").removeClass("mm-white");
+            }
+        });
+    };
+
+
     $(".m-nav--button").click(function() {
         var el = $(this);
         //el.parent().toggleClass("is-opened");
