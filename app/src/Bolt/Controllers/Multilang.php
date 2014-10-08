1b30e05b3c2803b640498c089be33338f2fcb9a7
Alexandr Hudeček <lexislav@gmail.com>
2014-10-08 19:36:12 +0200

    oprava controlleru – funguje maintenance mode

--- app/src/Bolt/Controllers/Multilang.php
+++ app/src/Bolt/Controllers/Multilang.php
@@ -13,6 +13,38 @@ class Multilang
      */
     public static function before(Request $request, Application $app)
     {
+        
+        // Start the 'stopwatch' for the profiler.
+        $app['stopwatch']->start('bolt.frontend.before');
+
+        // If there are no users in the users table, or the table doesn't exist. Repair
+        // the DB, and let's add a new user.
+        if (!$app['users']->getUsers()) {
+            //!$app['storage']->getIntegrityChecker()->checkUserTableIntegrity() ||
+            $app['session']->getFlashBag()->set('info', __('There are no users in the database. Please create the first user.'));
+
+            return redirect('useredit', array('id' => ''));
+        }
+
+        $app['debugbar'] = true;
+        $app['htmlsnippets'] = true;
+
+        // If we are in maintenance mode and current user is not logged in, show maintenance notice.
+        // @see /app/app.php, $app->error()
+        if ($app['config']->get('general/maintenance_mode')) {
+            if (!$app['users']->isAllowed('maintenance-mode')) {
+                $template = $app['config']->get('general/maintenance_template');
+                $body = $app['render']->render($template);
+
+                return new Response($body, 503);
+            }
+        }
+
+        // Stop the 'stopwatch' for the profiler.
+        $app['stopwatch']->stop('bolt.frontend.before');
+        
+        //začátek úpravy multilang
+        
         BoltFrontend::before($request, $app);
         $locales = $app['config']->get('general/i18n_locales', NULL);
 
@@ -21,6 +53,8 @@ class Multilang
                 $app->abort(404, $app['translator']->trans('Unknown or unsupported locale.'));
             }
         }
+        
+        
     }
 
     /**
